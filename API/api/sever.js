f0f9cad2da385f8fc516b621f2cd5f6c19a203f3
Daniel Souza <danieldesouza1024@gmail.com>
2025-06-04 12:35:24 -0300

    vendo se está saindo a porta

--- API/api/sever.js
+++ API/api/sever.js
@@ -1,20 +1,20 @@
 import express , {json} from 'express'
-import User from './Controllers/User.js'
-import privateRouter from './Routers/privateRouter.js'
-import Imovel from './Controllers/Imovel.js'
-import auth from './middleware/auth.js'
+import User from '../Controllers/User.js'
+import privateRouter from '../Routers/privateRouter.js'
+import Imovel from '../Controllers/Imovel.js'
+import auth from '../middleware/auth.js'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import comentario from './Controllers/Comentario.js'
-import paises from './Controllers/Paises.js'
-import tipos from './Controllers/Tipos.js'
-import tiposVenda from './Controllers/TipoVenda.js'
+import comentario from '../Controllers/Comentario.js'
+import paises from '../Controllers/Paises.js'
+import tipos from '../Controllers/Tipos.js'
+import tiposVenda from '../Controllers/TipoVenda.js'
 const app = express()
 app.use(cors())
 // app.use(cors({
 //   origin: 'https://venda-de-imovel.netlify.app',
 // }));
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 4000
 app.use(express.json())
 app.use("/",  comentario)
 app.use("/",  Imovel)
@@ -24,7 +24,7 @@ app.use("/", tipos)
 app.use("/", tiposVenda)
 app.use("/",auth, privateRouter)
 app.listen(PORT, ()=>{
-    console.log("Servidor rodando 🚀")
+    console.log("Servidor rodando 🚀", PORT)
 })
 //iUs8QhyJS3xze4k3
 //https://venda-de-imovel.netlify.app/
