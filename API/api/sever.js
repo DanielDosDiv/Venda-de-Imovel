2f3fa990014f2c5e1abc784629d9363f1e3f28fc
Daniel Souza <danieldesouza1024@gmail.com>
2025-06-04 12:44:47 -0300

    arrumando dnv

--- API/api/sever.js
+++ API/api/sever.js
@@ -1,30 +1,36 @@
-import express , {json} from 'express'
-import User from '../Controllers/User.js'
-import privateRouter from '../Routers/privateRouter.js'
-import Imovel from '../Controllers/Imovel.js'
-import auth from '../middleware/auth.js'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import comentario from '../Controllers/Comentario.js'
-import paises from '../Controllers/Paises.js'
-import tipos from '../Controllers/Tipos.js'
-import tiposVenda from '../Controllers/TipoVenda.js'
-const app = express()
-app.use(cors())
-// app.use(cors({
-//   origin: 'https://venda-de-imovel.netlify.app',
-// }));
-const PORT = process.env.PORT || 4000
-app.use(express.json())
-app.use("/",  comentario)
-app.use("/",  Imovel)
-app.use("/", User)
-app.use("/", paises)
-app.use("/", tipos)
-app.use("/", tiposVenda)
-app.use("/",auth, privateRouter)
-app.listen(PORT, ()=>{
-    console.log("Servidor rodando 🚀", PORT)
-})
-//iUs8QhyJS3xze4k3
-//https://venda-de-imovel.netlify.app/
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import User from '../Controllers/User.js';
+import privateRouter from '../Routers/privateRouter.js';
+import Imovel from '../Controllers/Imovel.js';
+import auth from '../middleware/auth.js';
+import comentario from '../Controllers/Comentario.js';
+import paises from '../Controllers/Paises.js';
+import tipos from '../Controllers/Tipos.js';
+import tiposVenda from '../Controllers/TipoVenda.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/", comentario);
+app.use("/", Imovel);
+app.use("/", User);
+app.use("/", paises);
+app.use("/", tipos);
+app.use("/", tiposVenda);
+app.use("/", auth, privateRouter);
+
+
+// app.listen(PORT, () => {
+//     console.log("Servidor rodando 🚀", PORT);
+// });
+
+//So usar isso para quando rodar o servidor localmente
+
+// exporta o app para Vercel
+export default app;
