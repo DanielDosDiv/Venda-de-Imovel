55d66572526f1e3a9f666c0bc50403123c958ce3
Daniel Souza <danieldesouza1024@gmail.com>
2025-06-05 11:06:14 -0300

    FINALMENTEEEEE, agora com o back-end no ar

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000'
+    baseURL: 'https://venda-imovel-api.onrender.com'
 });
 api.interceptors.request.use(config => {
     const token = localStorage.getItem("token");
