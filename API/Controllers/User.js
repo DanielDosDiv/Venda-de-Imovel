74c45c3b5d15cc0e091f341b63b70debc36a6131
Daniel Souza <danieldesouza1024@gmail.com>
2025-06-30 10:54:31 -0300

    ultimo teste

--- API/Controllers/User.js
+++ API/Controllers/User.js
@@ -12,16 +12,16 @@ router.post("/novoImovel", async (req, res) => {
 })
 router.post("/cadastro", async (req, res) => {
     const { name, email, password } = req.body
-    // if (!name || !email || !password) {
-    //     return res.status(401).json({ message: "Dados incompleots" })
-    // }
+    if (!name || !email || !password) {
+        return res.status(401).json({ message: "Dados incompleots" })
+    }
     try {
         const salt = await bcrypt.genSalt(10)
         const hashPassword = await bcrypt.hash(password, salt)
 
         const UserDb = await prisma.user.create({
             data: {
-                name: nome,
+                name: name,
                 email: email,
                 password: hashPassword
             }
